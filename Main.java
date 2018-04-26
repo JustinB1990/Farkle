c2b0164d57b9a787a2065bf111677eb37109ce6a
JustinB1990 <37849751+JustinB1990@users.noreply.github.com>
2018-04-26 15:50:21 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,23 +2,10 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        Hand hand = new Hand();
-        Dice diceOne = new Dice();
-        Dice diceTwo = new Dice();
-        Dice diceThree = new Dice();
-        Dice diceFour = new Dice();
-        Dice diceFive = new Dice();
-        Dice diceSix = new Dice();
-        hand.fillHand(diceOne, diceTwo, diceThree, diceFour, diceFive, diceSix);
-        hand.rollHand();
-        hand.showHand();
+    public static void main(String[] args) throws InterruptedException {
 
         Game game = new Game();
-        game.checkOnesAndFives(hand);
-
-        hand.showHand();
+        game.takeTurn();
 
 
 
