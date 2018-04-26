2057bdad5acb11948397bb481c3edf14c10e5a46
JustinB1990 <37849751+JustinB1990@users.noreply.github.com>
2018-04-26 15:52:14 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -14,9 +14,38 @@ public class Game {
         this.totalPoints = 0;
     }
 
-    public void takeTurn(){
+    public void takeTurn() throws InterruptedException {
 
+        boolean continueRolling = true;
+        boolean farkled = false;
+        Hand hand = new Hand();
 
+        while(continueRolling && !farkled){
+            boolean tookDice = false;
+            System.out.println("Rolling Dice...");
+            Thread.sleep(1000);
+            hand.rollHand();
+            if(!farkled){
+                while(!tookDice){
+                    int handCount = hand.getHandSize();
+                    hand = hand.sortHand(hand);
+                    hand.showHand();
+                    askForOnesAndFives(hand);
+                    turnPoints += handPoints;
+                    if(handCount == hand.getHandSize()){
+                        System.out.println("You didn't take any dice. You must take at least one.\n");
+                    } else {
+                        System.out.println("You have " + (hand.getHandSize()+1) + " dice remaining in hand for this turn and you've scored " + turnPoints + " points so far this turn.");
+                        continueRolling = continueTurn();
+                        tookDice = true;
+                    }
+                }
+            } else {
+                System.out.println("You Farkled. No points awarded this turn.");
+                turnPoints = 0;
+            }
+
+        }
 
     }
 
@@ -32,10 +61,10 @@ public class Game {
             System.out.println("Please enter 'Y' or 'N'.");
             answer = userInput.next();
 
-            if(answer.equals("Y")){
+            if(answer.equals("Y") || answer.equals("y")){
                 validInput = true;
                 continueTurn = true;
-            } else if(answer.equals("N")){
+            } else if(answer.equals("N") || answer.equals("n")){
                 validInput = true;
             } else {
                 System.out.println("You did not give a valid input.");
@@ -46,7 +75,7 @@ public class Game {
 
     }
 
-    public void checkOnesAndFives(Hand hand){
+    public void askForOnesAndFives(Hand hand){
 
         int diceToRemove = 0;
 
@@ -88,17 +117,17 @@ public class Game {
         boolean validInput = false;
         boolean takeDice = false;
 
-        System.out.println("Would you like to take a:" + diceValue + " for " + diceScore + " points?");
+        System.out.println("Would you like to take a: " + diceValue + " for " + diceScore + " points?");
 
         while(!validInput){
 
             System.out.println("Please enter 'Y' or 'N'.");
             answer = userInput.next();
 
-            if(answer.equals("Y")){
+            if(answer.equals("Y") || answer.equals("y")){
                 takeDice = true;
                 validInput = true;
-            } else if(answer.equals("N")){
+            } else if(answer.equals("N") || answer.equals("n")){
                 validInput = true;
             } else {
                 System.out.println("You did not give a valid input.");
@@ -109,5 +138,8 @@ public class Game {
         return takeDice;
 
     }
+
+
+
 
 }
