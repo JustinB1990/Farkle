a27d24b9a76637ac3df98cdc17f7fbba2c99d7df
JustinB1990 <37849751+JustinB1990@users.noreply.github.com>
2018-05-18 14:43:24 -0700

    Updated main from 5/17

--- Main.java
+++ Main.java
@@ -5,9 +5,12 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         Game game = new Game();
-        game.takeTurn();
+        while(game.getTotalPoints() < 10000) {
+            System.out.println("Turn: " + game.getTurnCounter());
+            game.takeTurn();
+        }
 
-
+        System.out.print("Total Points: " + game.getTotalPoints() + "\nTotal Turns Taken: " + game.getTurnCounter() + "\nTotal Farkles: " + game.getFarkleCounter());
 
     }
 }
